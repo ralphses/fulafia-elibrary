d9ee135820db8e3de13262fe4d860280a0561ff0
Raphael Eze <eze.raph@gmail.com>
2023-05-19 14:25:00 +0100

    initial commit

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('content')
+    <div id="admin-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h2 class="admin-heading">Settings</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="offset-md-3 col-md-6">
+                    <form class="yourform" action="{{ route('settings') }}" method="post" autocomplete="off">
+                        @csrf
+                        <div class="form-group">
+                            <label>Return Days</label>
+                            <input type="number" class="form-control" name="return_days"
+                                value="{{ $data->return_days ?? 0 }}" required>
+                            @error('return_days')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Fine (in Naira)</label>
+                            <input type="number" class="form-control" name="fine" value="{{ $data->fine ?? 0 }}"
+                                required>
+                            @error('fine')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <input type="submit" class="btn btn-danger" value="Update" required>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
